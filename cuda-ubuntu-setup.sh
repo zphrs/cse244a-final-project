a60db645340e578dc12223bb4fde9d0921002a88
zphrs <z@zephiris.dev>
2025-12-10 15:37:30 -0800

    updated cuda install script

---
+++ cuda-ubuntu-setup.sh
@@ -0,0 +1,3 @@
+sudo apt -y install gcc make
+wget https://developer.download.nvidia.com/compute/cuda/12.6.0/local_installers/cuda_12.6.0_560.28.03_linux.run
+sudo sh cuda_12.6.0_560.28.03_linux.run
